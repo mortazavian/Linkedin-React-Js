8b4e41f42f19cc03e0de16c418a19a7b80f23167
unknown <mehdi.mortazavian@gmail.com>
2023-08-05 12:58:38 +0330

    added add to your feed

---
+++ src/components/TopRightComponent/TopRightComponent.jsx
@@ -0,0 +1,16 @@
+import "./styles/TopRightComponent.css";
+
+function TopRightComponent() {
+  return (
+    <div className="top-right-component-container">
+      {/* Add to your feed 
+      3 instance of 3 pages
+      View all recommendations */}
+      <div className="add-to-your-feed-top-right-component">
+        Add to your feed
+      </div>
+    </div>
+  );
+}
+
+export default TopRightComponent;
