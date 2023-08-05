016db4d3811e5c067b98117352fb313eb4591f86
unknown <mehdi.mortazavian@gmail.com>
2023-08-05 13:03:43 +0330

    FollowInstance added to the project

---
+++ src/components/TopRightComponent/FollowInstance.jsx
@@ -0,0 +1,7 @@
+import "./styles/FollowInstance.css";
+
+const FollowInstance = () => {
+  return <div>hello</div>;
+};
+
+export default FollowInstance;
--- src/components/TopRightComponent/TopRightComponent.jsx
+++ src/components/TopRightComponent/TopRightComponent.jsx
@@ -1,14 +1,16 @@
 import "./styles/TopRightComponent.css";
+import FollowInstance from "./FollowInstance";
 
 function TopRightComponent() {
   return (
     <div className="top-right-component-container">
-      {/* Add to your feed 
-      3 instance of 3 pages
+      {/* Add to your feed  done
+      3 instance of 3 pages 
       View all recommendations */}
       <div className="add-to-your-feed-top-right-component">
         Add to your feed
       </div>
+      <FollowInstance></FollowInstance>
     </div>
   );
 }
