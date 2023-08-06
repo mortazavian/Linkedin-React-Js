b0f7253ef3c99f26f265c9c19e4e89ed5f4497d1
unknown <mehdi.mortazavian@gmail.com>
2023-08-06 14:15:01 +0330

    follow button added to the project

--- src/components/TopRightComponent/FollowButton.jsx
+++ src/components/TopRightComponent/FollowButton.jsx
@@ -1,9 +1,13 @@
 import "./styles/FollowButton.css";
+import plusIcon from "./../../assets/icons/TopRightComponentIcons/plusIcon.svg";
 
 const FollowButton = () => {
   return (
     <div>
-      <button className="follow-button-top-right-component"></button>
+      <button className="follow-button-top-right-component">
+        <img className="plus-icon-top-left-component" src={plusIcon}></img>
+        Follow
+      </button>
     </div>
   );
 };
