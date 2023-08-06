90d5d7e2ae979de730efb7c7cafd2a64982d46c7
unknown <mehdi.mortazavian@gmail.com>
2023-08-06 13:54:30 +0330

    name and picture added to top right component

--- src/components/TopRightComponent/FollowInstance.jsx
+++ src/components/TopRightComponent/FollowInstance.jsx
@@ -3,8 +3,13 @@ import "./styles/FollowInstance.css";
 const FollowInstance = (props) => {
   return (
     <div>
-      <div className="image-of-the-page-top-right-component">
-        <img src={props.image} alt="" />
+      <div className="two-part-container-top-right-component">
+        <div className="image-of-the-instance-of-page-top-right-component">
+          <img src={props.image} alt="" />
+        </div>
+        <div className="name-of-the-instance-of-page-top-right-component">
+          {props.name}
+        </div>
       </div>
     </div>
   );
--- src/components/TopRightComponent/TopRightComponent.jsx
+++ src/components/TopRightComponent/TopRightComponent.jsx
@@ -1,5 +1,8 @@
 import "./styles/TopRightComponent.css";
 import FollowInstance from "./FollowInstance";
+import instance1Picture from "../../assets/images/TopRightComponentImages/instance1.jpg";
+import instance2Picture from "../../assets/images/TopRightComponentImages/instance2.jpg";
+import instance3Picture from "../../assets/images/TopRightComponentImages/instance3.jpg";
 
 function TopRightComponent() {
   return (
@@ -10,7 +13,18 @@ function TopRightComponent() {
       <div className="add-to-your-feed-top-right-component">
         Add to your feed
       </div>
-      <FollowInstance></FollowInstance>
+      <FollowInstance
+        image={instance1Picture}
+        name={"Mohamad Mehdi Mohamadi"}
+      ></FollowInstance>
+      <FollowInstance
+        image={instance2Picture}
+        name={"Javad Javadian"}
+      ></FollowInstance>
+      <FollowInstance
+        image={instance3Picture}
+        name={"Ali Pishro"}
+      ></FollowInstance>
     </div>
   );
 }
