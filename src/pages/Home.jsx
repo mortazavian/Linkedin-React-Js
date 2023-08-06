22302138203f070aa7569e222614308fe22ccf87
unknown <mehdi.mortazavian@gmail.com>
2023-08-06 22:18:28 +0330

    background color added to index page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import TopLeftComponent from "../components/TopLeftComponent/TopLeftComponent";
 import TopMiddleComponent from "../components/TopMiddleComponent/TopMiddleComponent";
 import TopRightComponent from "../components/TopRightComponent/TopRightComponent";
+
+import "./styles/Home.css";
+
 const Home = () => {
   return (
     <div className="index-container-three-part">
